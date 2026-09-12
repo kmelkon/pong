00b4d319d05f65819edbb111ea5eed2cce9ec23e
Karam Malkon <kmelkon@gmail.com>
2026-09-12 21:09:00 +0200

    paddles are moving

--- ball.gd
+++ ball.gd
@@ -23,8 +23,8 @@ func _draw() -> void:
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	# if not is_on_floor():
+	# 	velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -33,6 +33,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	print("Direction: ", direction)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
---
+++ left_paddle.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("p1_up", "p1_down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
